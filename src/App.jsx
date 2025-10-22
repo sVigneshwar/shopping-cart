8a2c346a087fc8f3eb85ff8abaddcd5a4435212d
Vigneshwar <vigneshwar.s1996@gmail.com>
2025-10-23 01:19:21 +0530

    feat: Update routing for Shop page to use '/shopping-cart' path

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Shop />} />
+          <Route path="/shopping-cart" element={<Shop />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <Link to="/">Shop</Link>
+      <Link to="/shopping-cart">Shop</Link>
       <Link to="/cart">Cart</Link>
     </div>
   )
